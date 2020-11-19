8846ec1dce573d3181dbbfa6e146c5006d75fd22
Drew Hayward <d.andrew.hayward@gmail.com>
2020-11-19 18:46:22 -0500

    yuge speedups and stuff

--- euchre.py
+++ euchre.py
@@ -1,12 +1,16 @@
 
-from typing import List
+from typing import List, Dict, Optional
 from enum import Enum
-from copy import deepcopy
+from copy import deepcopy, copy
+import time
+from itertools import groupby
+from operator import itemgetter
 import random
 from functools import lru_cache
 from colorama import init as colorama_init
 colorama_init()
 from colorama import Fore, Back, Style
+from cfr import Game, Action as CFRAction, State as CFRState, CFR
 
 HIDDEN_CARD = """\
 ┌─────────┐
@@ -51,10 +55,10 @@ class Suit(Enum):
 
     def to_symbol(self) -> str:
         symbols = {
-            'Spades':   '♠',
-            'Diamonds': '♦',
-            'Hearts':   '♥',
-            'Clubs':    '♣',
+            'S':   '♠',
+            'D': '♦',
+            'H':   '♥',
+            'C':    '♣',
         }
         return symbols[self.value]
         
@@ -71,11 +75,29 @@ class Card:
         self.value = value
         self.suit = suit
 
+    def effective_suit(self, trump_suit):
+        if self.value == Value.Jack and self.suit == SUIT_COMPLEMENT[trump_suit]:
+            return trump_suit
+        else:
+            return self.suit
+
+    def __hash__(self):
+        return hash(self.get_code())
+
     def __eq__(self, other) -> bool:
         return self.value == other.value and self.suit == other.suit
     
     def __repr__(self) -> str:
         return f'{self.value.name} of {self.suit.name}'
+
+    def __lt__(self, other) -> bool:
+        if self.suit == other.suit:
+            return self.value.value < other.value.value
+        else:
+            return self.suit.value < other.suit.value
+
+    def get_code(self) -> str:
+        return self.suit.value + str(self.value.value)
 
     def pretty_card(self, color=True) -> str:
         if self.value.value < 3:
@@ -159,14 +181,19 @@ def print_cards(cards: List[Card]):
     print('\n'.join([' '.join(line) for line in lines]))
 
 @lru_cache(16)
-def get_card_rankings(trump_suit: Suit, lead_suit: Suit):
+def get_card_rankings(trump_suit: Suit, lead_suit: Optional[Suit]) -> Dict[Card, int]:
     """
-    Generates the card power rankings based on the current trump and lead suit
+    Generates the card power rankings based on the current trump and lead suit.
     """
     if trump_suit is None:
         raise InvalidStateException('Cannot get card rankings when trump has not been called')
     if lead_suit is None:
-        raise InvalidStateException('Cannot get card rankings when no card has been lead')
+        # Return a ranking so we can still group equivalent plays at the start of a trick
+        ranking = []
+        for suit in Suit:
+                ranking += [Card(val, suit) for val in reversed(Value)]
+        return {card: i for i, card in enumerate(ranking)}
+
 
     right_bower = Card(Value.Jack, trump_suit)
     left_bower = Card(Value.Jack, SUIT_COMPLEMENT[trump_suit])
@@ -191,7 +218,7 @@ def get_card_rankings(trump_suit: Suit, lead_suit: Suit):
 
     ranking += [left_bower, right_bower]
 
-    return ranking
+    return {card: i for i, card in enumerate(ranking)}
 
 ACTION_MAP = {
         Suit.Spades: {
@@ -235,7 +262,11 @@ SUIT_COMPLEMENT = {
     Suit.Hearts: Suit.Diamonds
 }
 
-class GameState:
+class EuchreGame(Game):
+    def get_num_players(self):
+        return 4
+
+class EuchreState(CFRState):
     """
     Team Even: Players 0 and 2
     Team Odd: Players 1 and 3
@@ -265,10 +296,50 @@ class GameState:
 
         # State for random sampling
         # Need to track if a player shows they are short suited
-        self.player_suits: List[List[Suit]]
+        #self.player_suits: List[List[Suit]]
 
         self._deal()
 
+        # Deal with spades always as trump
+        self.trump_suit = Suit.Spades
+        self.dealer = random.randint(0,3)
+        self.lead = (self.dealer + 1) % 4
+        self.current_agent = self.lead
+
+        # Skip tricks to test iteration
+        for i in range(1):
+            for play in range(4):
+                self.take_action(random.choice(self.valid_actions()), copy=False)
+
+    def __deepcopy__(self, memo):
+        # Create new instance
+        inst_copy = copy(self)
+
+        # Deepcopy containers
+        inst_copy.played_cards = self.played_cards[:]
+        inst_copy.player_hands = [hand[:] for hand in self.player_hands]
+        inst_copy.table = self.table[:]
+        inst_copy.kitty = self.kitty[:]
+        inst_copy.team_tricks = self.team_tricks[:]
+        inst_copy.team_points = self.team_points[:]
+        
+        return inst_copy
+
+    def get_current_agent(self) -> int:
+        return self.current_agent
+
+    def state_string(self):
+        return  f'{self.lead_suit.value if self.lead_suit else "0"}_' + ''.join(card.get_code() for card in self.player_hands[self.current_agent])
+
+    def is_terminal(self) -> bool:
+        """
+        If the calling team is set or has taken all the tricks, the hand is over
+        """
+        if self.calling_team is None:
+            return False
+        non_calling_team = 1 - self.calling_team
+        return self.team_tricks[self.calling_team] == 5 or self.team_tricks[non_calling_team] == 3
+            
     def _deal(self):
         deck = [Card(value, suit) for value in Value for suit in Suit]
         random.shuffle(deck)
@@ -279,9 +350,11 @@ class GameState:
             deck[10:15],
             deck[15:20]
         ]
+        for i, _ in enumerate(self.player_hands):
+            self.player_hands[i].sort()
 
         self.kitty: List[Card] = deck[20:]
-        self.player_suits = [[suit for suit in Suit] for _ in range(4)]
+        #self.player_suits = [[suit for suit in Suit] for _ in range(4)]
 
     def pretty_print_state(self):
         print(f'Team Even score: {self.team_points[0]}')
@@ -305,6 +378,19 @@ class GameState:
         hand = self.player_hands[self.current_agent]
         print_cards(hand)
        
+    def _group_equivalent_cards(self, hand: List[Card], ranking: Dict[Card, int], trump_suit: Suit):
+        hand_ranking = [(card, ranking[card]) for card in hand]
+        hand_ranking.sort(key=lambda x: x[1])
+        
+        card_groups = []
+        for k, g in groupby(
+            enumerate(hand_ranking), key=lambda x: (x[0] - x[1][1], x[1][0].effective_suit(trump_suit))
+        ):
+            group = list(pair[0] for pair in map(itemgetter(1), g))
+            card_groups.append(group)
+
+        return card_groups
+
     def valid_actions(self) -> List[Action]:
         hand = self.player_hands[self.current_agent]
         if max(self.team_points) >= 10:
@@ -323,27 +409,23 @@ class GameState:
         elif len(hand) > 5:
             return [act for act in Action if act.name.startswith('Discard')]
         else: # Playing the hand
-            if self.lead_suit is None:
-                # All cards are legal
-                return [ACTION_MAP[card.suit][card.value] for card in hand]
-
-            # Must follow suit if possible
-            left_bower = Card(Value.Jack, SUIT_COMPLEMENT[self.trump_suit])
-            follow_actions = [ACTION_MAP[card.suit][card.value] for card in hand if card.suit == self.lead_suit]
-            
-            # Left plays as trump
-            if self.lead_suit == self.trump_suit and left_bower in hand:
-                follow_actions.append(ACTION_MAP[left_bower.suit][left_bower.value])
-            
-            # Left cannot play as itself
-            if self.lead_suit == SUIT_COMPLEMENT[self.trump_suit] and left_bower in hand:
-                follow_actions.remove(ACTION_MAP[left_bower.suit][left_bower.value])
-
-            if follow_actions:
-                return follow_actions
+            playable_cards: List[Card] = []  
+            if self.lead_suit is not None:
+                follow_lead_cards = [card for card in hand if card.effective_suit(self.trump_suit) == self.lead_suit]
+                if any(follow_lead_cards):
+                    # Must follow suit if possible
+                    playable_cards = follow_lead_cards
+                else:
+                    playable_cards = hand
             else:
-                return [ACTION_MAP[card.suit][card.value] for card in hand]
+                # All cards are legal
+                playable_cards = hand
 
+            # Action abstraction
+            grouped_cards = self._group_equivalent_cards(playable_cards, get_card_rankings(self.trump_suit, self.lead_suit), self.trump_suit)
+            
+            return [ACTION_MAP[group[-1].suit][group[-1].value] for group in grouped_cards]
+            
     def take_action(self, action: Action, copy=True):
         if action not in self.valid_actions():
             raise InvalidActionExection(f'{action} is not valid in the current game state')
@@ -391,16 +473,16 @@ class GameState:
                     new_state.lead_suit = self.trump_suit
                 else:
                     new_state.lead_suit = played_card.suit
-            elif played_card.suit != self.lead_suit:
-                if self.lead_suit in new_state.player_suits[self.current_agent]:
-                    # The player must be shortsuited
-                    new_state.player_suits[self.current_agent].remove(self.lead_suit)
+            # elif played_card.suit != self.lead_suit:
+            #     if self.lead_suit in new_state.player_suits[self.current_agent]:
+            #         # The player must be shortsuited
+            #         new_state.player_suits[self.current_agent].remove(self.lead_suit)
             
             # Handle trick end logic
             if len(new_state.table) == 4:
-                rankings = get_card_rankings(new_state.trump_suit, new_state.lead_suit)
+                rankings: Dict[Card, int] = get_card_rankings(new_state.trump_suit, new_state.lead_suit)
                 trick: List[Card] = new_state.table.copy()
-                winning_card: Card = max(trick, key=lambda card: rankings.index(card))
+                winning_card: Card = max(trick, key=lambda card: rankings[card])
                 winning_player = ((trick.index(winning_card) + self.lead) % 4)
 
                 if self.debug:
@@ -418,26 +500,40 @@ class GameState:
                 new_state.current_agent = (self.current_agent + 1) % 4
 
             # End of hand logic
-            if all(len(hand) == 0 for hand in new_state.player_hands):
-                # award points
-                non_calling_team = (new_state.calling_team + 1) % 2
-                if new_state.team_tricks[new_state.calling_team] < new_state.team_tricks[non_calling_team]:
-                    new_state.team_points[non_calling_team] += 2
-                elif new_state.team_tricks[new_state.calling_team] == 5:
-                    new_state.team_points[new_state.calling_team] += 2
-                else:
-                    new_state.team_points[new_state.calling_team] += 1
+            # if all(len(hand) == 0 for hand in new_state.player_hands):
+            #     # award points
+            #     non_calling_team = (new_state.calling_team + 1) % 2
+            #     if new_state.team_tricks[new_state.calling_team] < new_state.team_tricks[non_calling_team]:
+            #         new_state.team_points[non_calling_team] += 2
+            #     elif new_state.team_tricks[new_state.calling_team] == 5:
+            #         new_state.team_points[new_state.calling_team] += 2
+            #     else:
+            #         new_state.team_points[new_state.calling_team] += 1
 
-                # deal new hand
-                new_state._deal()
-                new_state.trump_suit = None
-                new_state.hand_turn = 0
-                new_state.team_tricks = [0, 0]
-                new_state.dealer = (new_state.dealer + 1) % 4
-                new_state.lead = (new_state.dealer + 1) % 4
-                new_state.current_agent = (new_state.dealer + 1) % 4
+            #     # deal new hand
+            #     new_state._deal()
+            #     new_state.trump_suit = None
+            #     new_state.hand_turn = 0
+            #     new_state.team_tricks = [0, 0]
+            #     new_state.dealer = (new_state.dealer + 1) % 4
+            #     new_state.lead = (new_state.dealer + 1) % 4
+            #     new_state.current_agent = (new_state.dealer + 1) % 4
 
         return new_state
+
+    def get_utility(self, player_id: int) -> float:
+        player_team = player_id % 2
+        non_calling_team = 1 - self.calling_team
+        
+        points = [0, 0]
+        if self.team_tricks[non_calling_team] > self.team_tricks[self.calling_team]:
+            points[non_calling_team] = 2
+        elif self.team_tricks[self.calling_team] == 5:
+            points[self.calling_team] = 2
+        else:
+            points[self.calling_team] = 1
+
+        return points[player_team]
 
     def get_random_unseen(self):
         """
@@ -478,7 +574,7 @@ class GameState:
 
         return new_state
 
-def simulate_uniform(state: GameState):
+def simulate_uniform(state: EuchreState):
     starting_team = state.current_agent % 2
     opponent = (starting_team + 1) % 2
     starting_points = state.team_points.copy()
@@ -495,7 +591,7 @@ def simulate_uniform(state: GameState):
     return positive_points - negative_points
 
 
-def score_actions(state: GameState, num_samples=1000):
+def score_actions(state: EuchreState, num_samples=1000):
     actions = state.valid_actions()
     if len(actions) == 1:
         return actions, [None]
@@ -511,22 +607,29 @@ def score_actions(state: GameState, num_samples=1000):
 
 
 if __name__ == "__main__":
-    gs = GameState(debug=True)
-    while True:
-        print('-------------')
-        gs.pretty_print_state()
 
-        actions, rewards = score_actions(gs)
+    cfr = CFR(EuchreGame, EuchreState)
+    times = []
+    for i in range(10):
+        start = time.time()
+        cfr.train(1)
+        end = time.time()
 
-        for i, action in enumerate(actions):
-            print(f'{i+1}: {action}, Score: {rewards[i]}')
+        print('Total time for 1 iteration:', end-start)
+        times.append(end-start)
+    print('Mean time', sum(times)/10)
+    # suit_map = {
+    #     Suit.Clubs: Suit.Diamonds,
+    #     Suit.Diamonds: Suit.Clubs,
+    #     Suit.Spades: Suit.Hearts,
+    #     Suit.Hearts: Suit.Spades
+    # }
         
-        try:
-            id = int(input('Select action id:'))
-        except ValueError:
-            id = -1
+    # deck = [Card(value, suit) for value in Value for suit in Suit]
 
-        if not (1 <= id <= len(actions)):
-            id = random.choice(range(len(actions)))
-
-        gs = gs.take_action(actions[id-1])
+    # player_hands: List[List[Card]] = [
+    #         deck[:5],
+    #         deck[5:10],
+    #         deck[10:15],
+    #         deck[15:20]
+    #     ]
