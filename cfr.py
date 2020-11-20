3287639388f9ce4c58c28eb6a614836db64b83af
Drew Hayward <d.andrew.hayward@gmail.com>
2020-11-19 19:41:26 -0500

    speedups and fixes

--- cfr.py
+++ cfr.py
@@ -30,7 +30,7 @@ class InfoSet:
 
     def update_current_strategy(self):
         normalization = 0
-        for action, regret in self.cumulative_regrets:
+        for action, regret in self.cumulative_regrets.items():
             self.current_strategy[action] = regret if regret > 0 else 0
             normalization += self.current_strategy[action]
 
@@ -38,7 +38,7 @@ class InfoSet:
             for action in self.cumulative_regrets.keys():
                 self.current_strategy[action] = self.current_strategy[action] / normalization
         else:
-            self.current_strategy = {action: 1 / len(self.current_strategy)}
+            self.current_strategy = {action: 1 / len(self.current_strategy) for action in self.cumulative_regrets.keys()}
 
         
     def get_strategy_prob(self, action: Action) -> float:
@@ -58,13 +58,13 @@ class State:
     def get_current_agent(self) -> int:
         pass
 
-    def get_info_set(self) -> InfoSet:
-        pass
+    def get_info_set(self, valid_actions) -> InfoSet:
+        return InfoSet(valid_actions)
 
     def valid_actions(self) -> List[Action]:
         raise NotImplementedError()
 
-    def take_action(self, action: Action) -> State:
+    def take_action(self, action: Action, copy: bool = True):
         raise NotImplementedError()
 
     def is_terminal(self) -> bool:
@@ -94,28 +94,35 @@ class CFR:
         self._state_cls = state_cls
         self.game = game_cls()
         
-    def _cfr(self, agent_id, state: State, agent_path_probs: List[float]):
+    def _cfr(self, agent_id, state: State, agent_path_probs: List[float], depth=0):
         current_agent = state.get_current_agent()
         
         # Return terminal state payoff
         if state.is_terminal():
             return state.get_utility(agent_id)
+        valid_actions = state.valid_actions()
+
+        # Don't create an info set for states where the player has no choice
+        if len(valid_actions) == 1:
+            return self._cfr(agent_id, state.take_action(valid_actions[0], copy=False), agent_path_probs.copy(), depth=depth+1)
 
         # Get info set
         info_set_key = state.state_string()
         info_set = self.infoSet_map.get(info_set_key)
         if info_set is None:
-            info_set = state.get_info_set()
+            #print(f'Info set created for hand {info_set_key}')
+            info_set = state.get_info_set(valid_actions)
+            self.infoSet_map[info_set_key] = info_set
 
         utility = 0
         action_utils = {}
-        for action in state.valid_actions():
+        for action in valid_actions:
             action_prob = info_set.get_strategy_prob(action)
             
             new_path_probs = agent_path_probs.copy()
             new_path_probs[current_agent] = new_path_probs[current_agent] * action_prob
 
-            action_utils[action] = self._cfr(agent_id, state.take_action(action), new_path_probs)
+            action_utils[action] = self._cfr(agent_id, state.take_action(action), new_path_probs, depth=depth+1)
             utility += action_prob * action_utils[action]
 
         if current_agent == agent_id:
@@ -124,7 +131,7 @@ class CFR:
                 if i != agent_id:
                     non_player_prob *= prob
 
-            for action in state.valid_actions():
+            for action in valid_actions:
                 info_set.cumulative_regrets[action] += non_player_prob * (action_utils[action] - utility)
                 info_set.cumulative_strategy[action] += agent_path_probs[agent_id] * info_set.get_strategy_prob(action)
 
